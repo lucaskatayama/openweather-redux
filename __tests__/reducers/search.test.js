694458f397832740afd2d94e2d14d0c28dd580b8
Lucas Katayama <lucaskatayama@gmail.com>
2016-11-20 16:33:27 -0200

    fix(tests): fixing tests

--- __tests__/reducers/search.test.js
+++ __tests__/reducers/search.test.js
@@ -6,17 +6,18 @@ describe('Weather Reducer', () => {
   it('should have weather as empty object on start state', () => {
     let b;
     const newState = reducer(b, { type: '' });
-    expect(newState).to.be.eql({});
+    expect(newState).to.be.eql({ loading: false, results: null });
   });
-  it('should have weather updated on WEATHER_FETCH_SUCCESS', () => {
+  it('should have weather updated on WEATHER_SEARCH_SUCCESS', () => {
     const state = {};
     const action = {
-      type: types.WEATHER_FETCH_SUCCESS,
+      type: types.WEATHER_SEARCH_SUCCESS,
       payload: {
         city: '123',
       },
     };
     const newState = reducer(state, action);
-    expect(newState.city).to.be.eql('123');
+    expect(newState.loading).to.be.eql(false);
+    expect(newState.results).to.be.eql({ city: '123' });
   });
 });
